56ba0685df1598cbf2940c7a6a29b8495c7a3eda
Toni <tonirosinol@hotmail.com>
2016-11-13 19:37:54 +0100

    Rename folders and add exo4

--- exercise3 - Simple Tracker/code/matchDescriptors.m
+++ exercise3 - Simple Tracker/code/matchDescriptors.m
@@ -1,4 +1,4 @@
-function matches = matchDescriptors(...
+function unique_matches = matchDescriptors(...
     query_descriptors, database_descriptors, lambda)
 % Returns a 1xQ matrix where the i-th coefficient is the index of the
 % database descriptor which matches to the i-th query descriptor.
@@ -8,6 +8,18 @@ function matches = matchDescriptors(...
 % with an SSD < lambda * min(SSD). No two non-zero elements of matches will
 % be equal.
 
+[dists,matches] = pdist2(database_descriptors', query_descriptors', 'euclidean', 'Smallest', 1);
 
+sorted_dists = sort(dists);
+sorted_dists = sorted_dists(sorted_dists~=0);
+min_non_zero_dist = sorted_dists(1);
+
+matches(dists >= lambda * min_non_zero_dist) = 0;
+ 
+% remove double matches
+unique_matches = zeros(size(matches));
+[~,unique_match_idxs,~] = unique(matches, 'stable');
+unique_matches(unique_match_idxs) = matches(unique_match_idxs);
 
 end
+
