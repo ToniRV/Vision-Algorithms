0239c153ef68b426adfd2ce73997b0b21db75a4f
Toni <tonirosinol@hotmail.com>
2016-11-13 13:06:31 +0100

    First solution for describeKeypoints function exo 3

--- exercise3/code/describeKeypoints.m
+++ exercise3/code/describeKeypoints.m
@@ -2,7 +2,14 @@ function descriptors = describeKeypoints(img, keypoints, r)
 % Returns a (2r+1)^2xN matrix of image patch vectors based on image
 % img and a 2xN matrix containing the keypoint coordinates.
 % r is the patch "radius".
-
-
-
+tic
+    rows = keypoints(1,:)+r;
+    cols = keypoints (2,:)+r;
+    patch_size = (2*r+1) ^ 2;
+    descriptors = uint8(zeros(patch_size, length(rows)));
+    tmp_img = padarray(img, [r, r]);
+    for i=1:length(rows)
+        descriptors(:,i) = reshape(tmp_img(rows(i)-r:rows(i)+r, cols(i)-r:cols(i)+r), patch_size, 1);
+    end
+    disp(['Mine descript ' num2str(toc)]);
 end
