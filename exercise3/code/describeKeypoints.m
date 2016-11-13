2d364a120d39435461e4737717188df09298d46f
Toni <tonirosinol@hotmail.com>
2016-11-13 13:42:45 +0100

    Improve speed

--- exercise3/code/describeKeypoints.m
+++ exercise3/code/describeKeypoints.m
@@ -2,14 +2,14 @@ function descriptors = describeKeypoints(img, keypoints, r)
 % Returns a (2r+1)^2xN matrix of image patch vectors based on image
 % img and a 2xN matrix containing the keypoint coordinates.
 % r is the patch "radius".
-tic
     rows = keypoints(1,:)+r;
     cols = keypoints (2,:)+r;
-    patch_size = (2*r+1) ^ 2;
-    descriptors = uint8(zeros(patch_size, length(rows)));
+    N_features = length(rows);
+    patch_size = 2*r+1;
+    descriptors = repmat(uint8(zeros(patch_size)), 1, 1, N_features);
     tmp_img = padarray(img, [r, r]);
-    for i=1:length(rows)
-        descriptors(:,i) = reshape(tmp_img(rows(i)-r:rows(i)+r, cols(i)-r:cols(i)+r), patch_size, 1);
+    for i=1:N_features
+        descriptors(:,:,i) = tmp_img(rows(i)-r:rows(i)+r, cols(i)-r:cols(i)+r);
     end
-    disp(['Mine descript ' num2str(toc)]);
+    descriptors = reshape(descriptors, patch_size^2, N_features);
 end
