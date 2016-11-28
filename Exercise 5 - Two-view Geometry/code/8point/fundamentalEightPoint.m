cf5708a6d0a47c4daf7fd6e7b67e49a1d7973455
Toni <tonirosinol@hotmail.com>
2016-11-28 23:07:09 +0100

    Add solution for 8point algorithm

--- Exercise 5 - Two-view Geometry/code/8point/fundamentalEightPoint.m
+++ Exercise 5 - Two-view Geometry/code/8point/fundamentalEightPoint.m
@@ -12,8 +12,31 @@
 %
 % Output:
 %  - F(3,3) : fundamental matrix
-
-
 function F = fundamentalEightPoint(p1,p2)
 
+    [dim,NumPoints] = size(p1);
+    [dim2,NumPoints2] = size(p2);
+
+    % Sanity checks
+    assert(dim==dim2 && NumPoints==NumPoints2,'Size mismatch of input points');
+    assert(dim==3,'Input arguments are not 2D points');
+    assert(NumPoints>=8,'Insufficient number of points to compute fundamental matrix (need >=8)');
+
+    % Compute the measurement matrix A of the linear homogeneous system whose
+    % solution is the vector representing the fundamental matrix.
+    A = zeros(NumPoints,9);
+    for i=1:NumPoints
+        A(i,:) = kron(p1(:,i),p2(:,i)).';
+    end
+
+    % "Solve" the linear homogeneous system of equations A*f = 0.
+    % The correspondences x1,x2 are exact <=> rank(A)=8 -> there exist an exact solution
+    % If measurements are noisy, then rank(A)=9 => there is no exact solution, seek a least-squares solution.
+    [~,~,V] = svd(A,0);
+    F = reshape(V(:,9),3,3);
+
+    % Enforce det(F)=0 by projecting F onto the set of 3x3 singular matrices
+    [u,s,v]=svd(F);
+    s(3,3)=0;
+    F=u*s*v';
 end
